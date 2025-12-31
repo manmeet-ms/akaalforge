941adfec6233856de753390e5ae52b405ac0ad04
Manmeet Singh <manmeets.zsh@gmail.com>
2025-12-31 16:09:02 +0530

    removedreact amrdown import from the page and removed the detailed pages, dependent on dev.to blogs

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git status --short
 git add -A
-git commit -am "patch applied for latest vulnerability :: rm backend folder"
+git commit -am "removedreact amrdown import from the page and removed the detailed pages, dependent on dev.to blogs "
 git push origin main
